076692273289272dd31efee1f35a6d90bb3f2ac8
Matheus Monteiro <msmonteir@gmail.com>
2024-06-20 18:14:48 -0300

    feat: added enum to refer a type of measurement

--- src/app/modules/models/IMeasurement.ts
+++ src/app/modules/models/IMeasurement.ts
@@ -1,10 +1,21 @@
 import { IPatient } from "./IPatient";
 
+export enum TypeMeasurement {
+  Frequencia_Cardiaca,
+  Temperatura,
+  Frequencia_Respiratoria,
+  Pressao_Arterial,
+  Dor,
+  Glicemia_Capilar,
+  Saturacao_Oxigenio,
+}
+
 export interface IMeasurement {
   id: number;
   patientId: number;
-  type: string;
+  type: TypeMeasurement;
   value: number;
   timestamp: Date;
   patient: IPatient;
 }
+
